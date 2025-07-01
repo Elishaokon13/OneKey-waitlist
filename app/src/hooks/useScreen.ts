ce2d6837446d9f2e0d3895b57fea638eb6641cb3
Elishaokon13 <eliscodavid@gmail.com>
2025-07-01 19:12:27 +0100

    Modified src/hooks/useScreen.ts

--- app/src/hooks/useScreen.ts
+++
@@ -1,9 +0,0 @@
-import { useMediaQuery } from 'usehooks-ts'
-
-export function useScreenSize() {
-  const isMobile = useMediaQuery('(max-width: 767px)')
-  const isTabletAndAbove = useMediaQuery('(min-width: 768px)')
-
-  return { isMobile, isTabletAndAbove }
-}
-
