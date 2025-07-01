507bd292c07d8824635cb05bb968309b6a517312
Elishaokon13 <eliscodavid@gmail.com>
2025-07-01 19:25:46 +0100

    Modified src/components/hero.tsx

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Link from "next/link";
 import { motion } from "framer-motion";
 import GridIllustration from "./ui/grid-illustration";
 import IntegrationBox from "./ui/integrationBox";
