eb8fd9c39e0383b617e051e0c536addbecfbf828
Elishaokon13 <eliscodavid@gmail.com>
2025-07-01 19:25:08 +0100

    Modified .next/static/chunks/src_app_page_tsx_bacea2._.js

--- .next/static/chunks/src_app_page_tsx_bacea2._.js
+++ .next/static/chunks/src_app_page_tsx_bacea2._.js
@@ -5,10 +5,10 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
   "path": "static/chunks/src_app_page_tsx_bacea2._.js",
   "chunks": [
-    "static/chunks/src_cd1d00._.js",
+    "static/chunks/src_6cca0b._.js",
     "static/chunks/node_modules_next_b453e2._.js",
     "static/chunks/node_modules_framer-motion_dist_es_6d2d35._.js",
-    "static/chunks/node_modules_922c10._.js"
+    "static/chunks/node_modules_c424ec._.js"
   ],
   "source": "dynamic"
 });
